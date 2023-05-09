310004fc732583758b247dd453c3c3db94ff9b17
Maryna <komunarmarin@gmail.com>
2023-05-09 22:07:18 +0300

    add.hw.js.08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,45 +1,39 @@
-import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox'; // Видаліть /dist/simple-lightbox.min.js з імпорту
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
 
-const galleryEl = document.querySelector('.gallery');
-const imgGallery = createImgCard(galleryItems);
+const gallery = document.querySelector('.gallery');
 
-galleryEl.insertAdjacentHTML('beforeend', imgGallery);
-
-function createImgCard(galleryItems) {
-return galleryItems
-    .map(({ preview, original, description }) => `
-    <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-    </li>
-    `)
-    .join('');
+function createGalleryItem({ preview, original, description }) {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `;
 }
 
-function onImgOriginslCard(event) {
-event.preventDefault();
+function renderGalleryItems(galleryItems) {
+    const galleryItemsMarkup = galleryItems
+        .map(item => createGalleryItem(item))
+        .join('');
 
-if (event.target.nodeName !== 'IMG') {
-    return;
+    gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 }
 
-const instance = new SimpleLightbox('.gallery a', {
-    onShow: () => document.addEventListener('keydown', onCloseModal),
-    onClose: () => document.removeEventListener('keydown', onCloseModal),
+renderGalleryItems(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
 });
 
-instance.open();
-}
-
-function onCloseModal(event) {
-if (event.code === 'Escape') {
-    instance.close();
-}
-}
-
-galleryEl.addEventListener('click', onImgOriginslCard);
+gallery.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') {
+        lightbox.open();
+    }
+});
